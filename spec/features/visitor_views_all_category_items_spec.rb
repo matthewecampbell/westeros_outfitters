81bd0ff8f4254a43202824a3a0cac05e6fc0deb7
Garrett <garrett.smestad@gmail.com>
2016-07-26 19:20:33 -0600

    Spec for category show page
    Created spec to test individual category's show page displays all items in that category.

--- spec/features/visitor_views_all_category_items_spec.rb
+++ spec/features/visitor_views_all_category_items_spec.rb
@@ -3,18 +3,18 @@
 # I see all items assigned to that category
 require 'rails_helper'
 
-RSpec.feature "Visitor views all items in category" do
-  scenario "they see the names of each item" do
-    category = Category.create(name: "Weapons")
+RSpec.feature 'Visitor views all items in category' do
+  scenario 'they see the names of each item' do
+    category = Category.create(name: 'Weapons')
 
-    items = %w(Sword Shield Bow).map do |item_name|
-      category.items.create(name: item_name, img: "https://pbs.twimg.com/profile_images/715136295933255680/5XIOqSKw.jpg", price: 9.99)
+    %w(Sword Shield Bow).map do |item_name|
+      category.items.create(name: item_name, img: 'https://pbs.twimg.com/profile_images/715136295933255680/5XIOqSKw.jpg', price: 9.99)
     end
 
     visit category_path(category)
 
-    expect(page).to have_content("Sword")
-    expect(page).to have_content("Shield")
-    expect(page).to have_content("Bow")
+    expect(page).to have_content('Sword')
+    expect(page).to have_content('Shield')
+    expect(page).to have_content('Bow')
   end
 end

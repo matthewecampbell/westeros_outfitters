f59b64acb98d666a027c291f1af9a2999b2461f6
Nicole Marino <nicolekmarino@hotmail.com>
2016-07-28 15:39:02 -0600

    added feature for retired items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727190319) do
+ActiveRecord::Schema.define(version: 20160728204737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20160727190319) do
     t.string   "name"
     t.string   "img"
     t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "category_id"
     t.text     "description"
+    t.boolean  "retired",     default: false
   end
 
   add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree

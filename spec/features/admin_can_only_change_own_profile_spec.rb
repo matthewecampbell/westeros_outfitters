5d10cd018b53587597820824add4641346acccd0
Garrett <garrett.smestad@gmail.com>
2016-07-31 23:20:06 -0600

    Fixed failing test
    I'd changed the way @user is set in admin/users_controller.rb which was causing the test to fail
    Changed it around so now all tests pass and everything is back to being functional.

--- spec/features/admin_can_only_change_own_profile_spec.rb
+++ spec/features/admin_can_only_change_own_profile_spec.rb
@@ -1,19 +1,19 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "admin goes to dashboard" do
-  scenario "can only edit own info" do
-    user = User.create!(id: 1, username: "ned", password: "password", address: "12", email: "132", role: 1)
-    user = User.create!(id: 2, username: "ned1", password: "password", address: "12", email: "1132", role: 1)
+RSpec.feature 'admin goes to dashboard' do
+  scenario 'can only edit own info' do
+    user = User.create!(id: 1, username: 'ned', password: 'password', address: '12', email: '132', role: 1)
+    user2 = User.create!(id: 2, username: 'ned1', password: 'password', address: '12', email: '1132', role: 1)
 
-    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    ApplicationController.any_instance.stubs(:current_user).returns(user2)
 
     visit admin_dashboard_path
 
     expect(current_path).to eq(admin_dashboard_path)
 
-    click_link "Edit User Profile"
-    expect(current_path).to eq("/admin/users/2/edit")
-    visit "/admin/users/1/edit"
+    click_link 'Edit User Profile'
+    expect(current_path).to eq('/admin/users/2/edit')
+    visit '/admin/users/1/edit'
     expect(page).to have_content("The page you were looking for doesn't exist")
   end
 end

9c473d71a35a6297f8e3641645af16461777ddc1
Zack Forbing <zackforbing@gmail.com>
2016-08-03 17:22:08 -0600

    test complete

--- spec/features/user_can_place_an_order_spec.rb
+++ spec/features/user_can_place_an_order_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature 'User can place an order' do
-  scenario 'they add two items to their cart and check out' do skip
+  scenario 'they add two items to their cart and check out' do
 
     category = Category.create!(name: 'weapons')
-    category.items.create!(id: 15, name: 'Ice', img: 'http://www.valyriansteel.com/shop/images/uploads/ice-main.jpg', price: 0.99, description: "it's cold")
-    category.items.create!(id: 16, name: 'Ice2', img: 'http://www.valyriansteel.com/shop/images/uploads/ice-main2.jpg', price: 2.99, description: "it's colder")
+    item = category.items.create!(id: 15, name: 'Ice', img: 'http://www.valyriansteel.com/shop/images/uploads/ice-main.jpg', price: 0.99, description: "it's cold")
     user = User.create!(id: 1, username: 'ned', password: 'stark', email: 'raven@raven.net', address: 'winterfell')
     ApplicationController.any_instance.stubs(:current_user).returns(user)
-    cart = Cart.new({ '15' => 1, '16' => 2 })
+
+    visit item_path(item)
+
+    click_button 'Add to Cart'
 
     visit cart_path
-    require "pry"; binding.pry
-    click_on "Checkout"
+    click_button 'Create Order'
+
+    expect(current_path).to eq("/orders/3")
+    expect(page).to have_content("Order Number: 3")
+    expect(page).to have_content("Order Status: Ordered")
+    expect(page).to have_content("Amount: $0.99")
   end
 end

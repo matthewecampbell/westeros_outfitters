624bf29235b8cc18c6f3de7b0e085dc0de92ea01
Garrett <garrett.smestad@gmail.com>
2016-08-03 23:24:34 -0600

    Edited error page rendering
    Added a conditional to the categories controller to handle any .com/'fakestuff' type errors
    Added an ActiveRecord error handler in the application controller for moments when the error is caused by ActiveRecord::RecordNotFound.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
   helper_method :current_user, :current_admin?
+  rescue_from ActiveRecord::RecordNotFound, :with => :rescue_action
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -13,5 +14,17 @@ class ApplicationController < ActionController::Base
 
   def set_cart
     @cart = Cart.new(session[:cart])
+  end
+
+  private
+
+  # handles 404 when a record is not found.
+  def rescue_action(exception)
+    case exception
+    when ActiveRecord::RecordNotFound, ActionController::UnknownAction, ActionController::RoutingError
+      render :file => "app/views/errors/not_found.html.erb", :status => 404
+    else
+      super
+    end
   end
 end
